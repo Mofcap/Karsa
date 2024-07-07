802a1108b535f201d53fb57d0ebd5552ad3943db
Maouchi <fouad.maouchi.96@gmail.com>
2024-07-07 17:41:42 +0200

    fin

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import './NotFound.css';
 const NotFound = () => (
   <div className="not-found">
     <h1 className='h1notfount'>404</h1>
-    <p className='parnotfound'>Oups! La page que vous demandez n'existe pas.</p>
+    <p className='parnotfound'>Oups! La  page que vous demandez n'existe pas.</p>
     <Link className='return' to="/">Retourner à la page d'accueil</Link>
   </div>
 );
